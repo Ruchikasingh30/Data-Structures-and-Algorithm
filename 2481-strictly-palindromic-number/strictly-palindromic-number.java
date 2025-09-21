07af03f694223b77cb2f7a0f5a88de30554c7547
Ruchikasingh30 <167904140+Ruchikasingh30@users.noreply.github.com>
2025-09-21 23:10:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (12.83%) - LeetSync

---
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        // No number >= 4 is strictly palindromic in all bases 2 to n - 2
+        return false;
+    }
+}
