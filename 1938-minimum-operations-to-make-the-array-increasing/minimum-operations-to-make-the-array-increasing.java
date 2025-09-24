916f06ad07ba18b88b9ad5fbd8fc56b92f45bd57
Ruchikasingh30 <167904140+Ruchikasingh30@users.noreply.github.com>
2025-09-24 19:59:48 +0530

    Time: 3 ms (89.43%) | Memory: 45.4 MB (27.03%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -1,14 +1,17 @@
 class Solution {
     public int minOperations(int[] arr) {
-        int sum=0;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]<arr[i+1]) continue;
-            else{
-                int toadd = arr[i]-arr[i+1]+1;
-                arr[i+1] = toadd +arr[i+1];
-                sum= sum+toadd;
+        int operations = 0;
+        int prev = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] <= prev) {
+                int needed = prev + 1 - arr[i];
+                operations += needed;
+                arr[i] = prev + 1;
             }
+            prev = arr[i];
         }
-        return sum;
+        
+        return operations;
     }
 }
