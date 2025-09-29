b264903aacc3b98f98b45111dacedd45b953b0d8
Ruchikasingh30 <167904140+Ruchikasingh30@users.noreply.github.com>
2025-09-29 13:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (91.71%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,18 +3,7 @@ class Solution {
         if (s.length() != goal.length()) {
             return false;
         }
-        String doubled = s + s; //concats the string.
-        
-        int g = goal.length();
-        for (int i = 0; i <= doubled.length() - g; i++) {
-            int j = 0;
-            while (j < g && doubled.charAt(i + j) == goal.charAt(j)) {
-                j++;
-            }
-            if (j == g) {
-                return true;
-            }
-        }
-        return false;
+        String doubled = s + s;
+        return doubled.contains(goal);
     }
 }
