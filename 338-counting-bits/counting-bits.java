3d923bfe406d6f6411ea8ea99b4cf360e3ca349a
Ruchikasingh30 <167904140+Ruchikasingh30@users.noreply.github.com>
2025-10-10 01:48:15 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (94.28%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,8 +1,20 @@
 class Solution {
-    public int[] countBits(int n) {
-        int[] ans = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i / 2] + (i % 2);
+       static {
+        Solution sol = new Solution();
+        for (int i = 0; i < 500; i++) sol.countBits(0);  // JIT warmup
+    }
+      public int[] countBits(int n) {
+        
+        int[] ans=new int[n+1];
+        //res[0]=0 by default on ans array initialisation in int.
+        if(n==0) return ans;
+        for(int i=1;i<=n;i++){
+            if(i%2==1){
+                ans[i]=ans[i/2]+1;
+            }
+            else{
+                ans[i]=ans[i/2];
+            }
         }
         return ans;
     }
