3213fff7475e301349736b6ea06852e0b4c33c30
Ruchikasingh30 <167904140+Ruchikasingh30@users.noreply.github.com>
2025-10-26 23:28:12 +0530

    Time: 97 ms (68.95%) | Memory: 101.4 MB (37.74%) - LeetSync

---
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -0,0 +1,43 @@
+class Bank {
+    private long[] balance;
+
+    public Bank(long[] balance) {
+        this.balance = balance;
+    }
+
+    private boolean isValid(int account) {
+        // Accounts are numbered from 1 to n, balance is 0-indexed
+        return account >= 1 && account <= balance.length;
+    }
+
+    public boolean transfer(int account1, int account2, long money) {
+        if (!isValid(account1) || !isValid(account2)) {
+            return false;
+        }
+        if (balance[account1 - 1] < money) {
+            return false;
+        }
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    public boolean deposit(int account, long money) {
+        if (!isValid(account)) {
+            return false;
+        }
+        balance[account - 1] += money;
+        return true;
+    }
+
+    public boolean withdraw(int account, long money) {
+        if (!isValid(account)) {
+            return false;
+        }
+        if (balance[account - 1] < money) {
+            return false;
+        }
+        balance[account - 1] -= money;
+        return true;
+    }
+}
