8909fa93983a76a1d7ba5208c158a85f25853f4c
Ruchikasingh30 <167904140+Ruchikasingh30@users.noreply.github.com>
2025-11-04 20:44:59 +0530

    Time: 3 ms (100.00%) | Memory: 46.7 MB (7.59%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.java
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.java
@@ -1,58 +1,52 @@
 class Solution {
     public int[] findXSum(int[] nums, int k, int x) {
         int n = nums.length;
-        // The result array will have n - k + 1 elements
-        int[] ans = new int[n - k + 1];
-        
-        // Iterate through the start of each window
-        for (int i = 0; i <= n - k; i++) {
-            
-            // 1. Create a frequency map for the current window
-            Map<Integer, Integer> freqMap = new HashMap<>();
-            int currentWindowSum = 0;
-            for (int j = i; j < i + k; j++) {
-                freqMap.put(nums[j], freqMap.getOrDefault(nums[j], 0) + 1);
-                currentWindowSum += nums[j]; // Also calculate total sum for edge case
+        int[] result = new int[n - k + 1];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = calculateXSumForSubarray(nums, i, i + k - 1, x);
+        }
+
+        return result;
+    }
+
+    private int calculateXSumForSubarray(int[] nums, int start, int end, int x) {
+        final int MAX_VALUE = 50;
+        int[] frequency = new int[MAX_VALUE + 1];
+        int distinctCount = 0;
+
+        int totalSum = 0;
+        for (int i = start; i <= end; i++) {
+            int num = nums[i];
+            totalSum += num;
+            if (frequency[num] == 0) {
+                distinctCount++;
             }
-            
-            // 2. Handle the edge case (fewer than x distinct elements)
-            if (freqMap.size() < x) {
-                ans[i] = currentWindowSum;
-                continue;
-            }
-            
-            // 3. Convert map entries to a list
-            List<Map.Entry<Integer, Integer>> entries = new ArrayList<>(freqMap.entrySet());
-            
-            // 4. Sort the list
-            //     Sort by frequency (value) descending
-            //     If frequencies are equal, sort by element (key) descending
-            Collections.sort(entries, (a, b) -> {
-                if (a.getValue() != b.getValue()) {
-                    return b.getValue() - a.getValue(); // Descending frequency
-                } else {
-                    return b.getKey() - a.getKey();     // Descending value
-                }
-            });
-            
-            // 5. Get the top x elements into a Set for fast lookup
-            Set<Integer> topXElements = new HashSet<>();
-            for (int j = 0; j < x; j++) {
-                topXElements.add(entries.get(j).getKey());
-            }
-            
-            // 6. Calculate the final x-sum for the window
-            int xSum = 0;
-            for (int j = i; j < i + k; j++) {
-                if (topXElements.contains(nums[j])) {
-                    xSum += nums[j];
+            frequency[num]++;
+        }
+
+        if (distinctCount < x) {
+            return totalSum;
+        }
+
+        int resultSum = 0;
+        for (int selection = 0; selection < x; selection++) {
+            int bestNumber = -1;
+            int bestFrequency = -1;
+
+            for (int number = MAX_VALUE; number >= 1; number--) {
+                if (frequency[number] > bestFrequency) {
+                    bestFrequency = frequency[number];
+                    bestNumber = number;
                 }
             }
-            
-            // 7. Store the sum
-            ans[i] = xSum;
+
+            if (bestNumber != -1) {
+                resultSum += bestNumber * bestFrequency;
+                frequency[bestNumber] = 0; 
+            }
         }
         
-        return ans;
+        return resultSum;
     }
 }
