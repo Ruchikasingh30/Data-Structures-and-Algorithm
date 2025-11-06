24a14410e6b77debde74b009780c67764743a5b4
Ruchikasingh30 <167904140+Ruchikasingh30@users.noreply.github.com>
2025-11-06 18:42:23 +0530

    Time: 2 ms (57.21%) | Memory: 43.2 MB (5.60%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,21 +1,18 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] charCounts = new int[26];
-    
+        char c = 0;
+        
+        // XOR all characters in string s
         for (int i = 0; i < s.length(); i++) {
-            charCounts[s.charAt(i) - 'a']++;
+            c = (char) (c ^ s.charAt(i));
         }
         
+        // XOR all characters in string t
         for (int i = 0; i < t.length(); i++) {
-            charCounts[t.charAt(i) - 'a']--;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (charCounts[i] == -1) {
-                return (char) (i + 'a');
-            }
+            c = (char) (c ^ t.charAt(i));
         }
         
-        return ' ';
+        // The remaining value in 'c' is the added character
+        return c;
     }
 }
